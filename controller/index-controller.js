5343fab0f7631c26568f191cbceb0e780f9e3b9f
Michael Brunner <brunner@cadwork.swiss>
2023-06-08 21:27:41 +0200

    implement first services

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,7 +1,15 @@
 export class IndexController {
     index(req, res) {
         res.render("index", {data: "Hello World", dark: true});
+        // res.redirect("/orders");
     };
+    createOrder(req, res) {
+        res.render("createOrder", {data: "Hello World", dark: true});
+    };
+
+    createPizza(req, res) {
+        res.render("succeeded", {data: "Hello World", dark: true});
+    }
 }
 
 export const indexController = new IndexController();
